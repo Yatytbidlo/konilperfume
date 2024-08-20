5808bd530c0f1c5709190e1e42d80d551b80ba37
Nik <123063678+Yatytbidlo@users.noreply.github.com>
2024-08-20 04:51:38 +0300

    init

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ except Exception as e:
 
 # Конфигурация базы данных
 db_config = {
-    'host': '127.0.0.1',
+    'host': '192.168.0.3',
     'user': 'root',
     'database': 'konil',
     'password': '',
